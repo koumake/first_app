ea0875799806eaee20aa900b2facc38dba0f681f
koumake <koumake422@gmail.com>
2020-07-20 14:53:33 +0900

    既読機能サーバーサイド

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,22 @@
 class PostsController < ApplicationController
   def index
-    @post = "これはコントローラーで定義したインスタンス変数を確認するための文字列です"
-
+    @post = Post.all.order(id: "DESC")
   end
   def new
    
   end
   def create
     @post = Post.create(content: params[:content])
+    redirect_to action: :index
+  end
+  def checked
+    post = Post.find(params[:id])
+    if post.checked
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+    item = Post.find(params[:id])
+    render json: {post: item}
   end
 end
